eb3f677730ccbbba933f2338476f2c6573d38ad8
Nothing-Lt <Nothing-Lt@users.noreply.github.com>
2017-05-04 09:47:36 +0800

    Update GetImportant.py

--- pm25/pm25/GetImportant.py
+++ pm25/pm25/GetImportant.py
@@ -2,6 +2,11 @@ import urllib
 
 strURL='http://192.168.43.106:8080/PM25PredictServer/getimport?city='
 
+if !os.path.exists('Location'):
+  print 'No Location'
+  exit()
+if os.path.exists('Important'): 
+  exit()
 strCity='Beijing'
 file_t=open('Location')
 strCityURL=strURL+file_t.readline()
